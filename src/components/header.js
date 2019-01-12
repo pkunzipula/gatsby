1e63dee38cd79a2d10ef4c79a6c67b175f4709eb
James Capra <jcapra76@gmail.com>
2019-01-11 18:47:17 -0800

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,4 @@
+import React from "react"
+export default props => <h1 style={{ color: `peru` }}>{props.headerText} {props.randomTask}</h1>
+
+//export default() => <h1>This is a not so much a header.</h1>
