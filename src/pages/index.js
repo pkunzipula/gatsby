8c685e251aeeb15f2da0fc7afe15d0e31dd7f182
pkunzipula <jcapra76@gmail.com>
2019-11-15 12:20:25 -0800

    Fixing styles
    Fixing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,18 @@ const divStyle = {
     textAlign: 'center',
     color: '#878787',
     fontFamily: 'Fira Sans',
-    fontSize: '1.625rem',
-    lineHeight: '2rem'
 };
 
 const h1Style = {
     fontWeight: 'normal',
+    fontSize: '1.5',
+    lineHeight: '1.8',
 };
+
+const pStyle = {
+    fontSize: '1.1',
+    lineHeight: '1.8'
+}
 
 const imgStyle = {
     lineHeight: '1',
@@ -31,7 +36,7 @@ export default () => (
     <div style={divStyle}>
         <img style={imgStyle} src={`../../biggerHair16x6.jpg`} alt="Ary and guests" />
         <h1 style={h1Style}>Hello and welcome to <span style={candyStyle}>Hair by Ary!</span></h1>
-        <p>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
+        <p style={pStyle}>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
             I'm always accepting new guests, so please contact me for an appointment!<br/>
             I can't wait to see you!</p>
     </div>
