081edc5125ed420dfc3f71ee78eae3a623db8d83
pkunzipula <jcapra76@gmail.com>
2019-11-15 10:42:53 -0800

    Hair by Ary
    The New Look of...

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import Header from "../components/header"
 export default () => (
     <div style={{ color: `purple`}}>
         <Link to="/contact/">Contact</Link>
-        <h1>Hello Gatsby!</h1>
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <img src="/static/biggerHair16x6.jpg" alt="Ary and guests" />
+        <h1>Hello and welcome to Hair by Ary!</h1>
+        <p>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
+            I'm always accepting new guests, so please contact me for an appointment!<br/>
+            I can't wait to see you!</p>
     </div>
 )
