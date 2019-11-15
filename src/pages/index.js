0fdb29a0306148b1e7e733b051232ed7f51d8e11
pkunzipula <jcapra76@gmail.com>
2019-11-15 11:03:20 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default () => (
     <div style={{ color: `purple`}}>
         <Link to="/contact/">Contact</Link>
-        <img src="../../static/biggerHair16x6.jpg" alt="Ary and guests" />
+        <img src="{../../biggerHair16x6.jpg}" alt="Ary and guests" />
         <h1>Hello and welcome to Hair by Ary!</h1>
         <p>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
             I'm always accepting new guests, so please contact me for an appointment!<br/>
