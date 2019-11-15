81e5bc6ab5963a080e23ccf5c15841fb7da8add3
pkunzipula <jcapra76@gmail.com>
2019-11-15 11:59:56 -0800

    More styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import 'typeface-emilys-candy';
-import 'typeface-fresca';
+import 'typeface-fira-sans-condensed';
 
 const divStyle = {
     margin: '20px auto',
     maxWidth: '1500px',
     textAlign: 'center',
     color: '#ccc',
-    fontFamily: 'Fresca',
+    fontFamily: 'Fira Sans Condensed',
     fontSize: '2rem',
-    lineHeight: '1.825rem'
+    lineHeight: '2.825rem'
+};
+
+const h1Style = {
+    fontWeight: 'normal',
 };
 
 const imgStyle = {
@@ -26,7 +30,7 @@ const candyStyle = {
 export default () => (
     <div style={divStyle}>
         <img style={imgStyle} src={`../../biggerHair16x6.jpg`} alt="Ary and guests" />
-        <h1>Hello and welcome to <span style={candyStyle}>Hair by Ary!</span></h1>
+        <h1 style={h1Style}>Hello and welcome to <span style={candyStyle}>Hair by Ary!</span></h1>
         <p>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
             I'm always accepting new guests, so please contact me for an appointment!<br/>
             I can't wait to see you!</p>
