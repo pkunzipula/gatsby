7f98fbcc4ec7d71636eb888b0e17b32930a63138
pkunzipula <jcapra76@gmail.com>
2019-11-15 11:15:55 -0800

    Styling the Index
    Added Div and Img styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 
+const divStyle = {
+    margin: '0 auto',
+    maxWidth: '1500px',
+    textAlign: 'center',
+    color: '#ccc'
+};
+
+const imgStyle = {
+    lineHeight: '1',
+    maxWidth: '100%'
+}
+
 export default () => (
-    <div style={{ color: `purple`}}>
-        <Link to="/contact/">Contact</Link>
-        <img src={`../../biggerHair16x6.jpg`} alt="Ary and guests" />
+    <div style={divStyle}>
+        <img style={imgStyle} src={`../../biggerHair16x6.jpg`} alt="Ary and guests" />
         <h1>Hello and welcome to Hair by Ary!</h1>
         <p>I'm at Sola Salon on the Southwest corner of Warm Springs and Buffalo.<br/>
             I'm always accepting new guests, so please contact me for an appointment!<br/>
